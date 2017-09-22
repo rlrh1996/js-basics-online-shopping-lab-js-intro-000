c3df4020726a72563f9e60eee4ce1095fcbc81db
rlrh1996 <rlrh1996@gmail.com>
2017-09-22 03:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -92,4 +92,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === null) {
+      console.log("Sorry, we don't have a credit card on file for you.");
+      return;
+  }
 }
