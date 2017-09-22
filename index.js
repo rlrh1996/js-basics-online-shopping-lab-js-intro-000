5e51766fdc485a67bc7469e8f3858472d9395038
rlrh1996 <rlrh1996@gmail.com>
2017-09-22 02:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    // write your code here
+    var itemPrice = Math.floor(Math.random() * 100);
+    cart.push({[item]: itemPrice});
+    console.log(`${item} has been added to your cart.`);
+    return cart;
 }
 
 function viewCart() {
   // write your code here
+    switch (cart.length) {
+        case 0:
+            console.log("Your shopping cart is empty.");
+        case 1:
+            var item = cart[0];
+            var keys = Object.keys(item);
+            var itemName = keys[0];
+            var itemPrice = item.keys[0];
+            console.log(`In your cart, you have ${itemName} at $${itemPrice}.`);
+        case 2:
+        default:
+    }
+
+    }
 }
 
 function total() {
