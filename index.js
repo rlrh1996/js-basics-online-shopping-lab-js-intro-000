98988a2de33bbf6cb8b093cd17e1cb0a214ed203
rlrh1996 <rlrh1996@gmail.com>
2017-09-22 03:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function placeOrder(cardNumber) {
         console.log("Sorry, we don't have a credit card on file for you.");
         return;
     } else {
-        let total = total();
+        var total = total();
         console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`);
         cart = [];
         return;
